821b4d7e03b9808bfd9281ffd963a86e0ec40db9
shaojung <shaojung@gmail.com>
2016-08-02 14:10:33 +0800

    Volley step 1

--- app/src/main/java/tw/com/pcschool/t080202/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/t080202/MainActivity.java
@@ -2,6 +2,13 @@ package tw.com.pcschool.t080202;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        StringRequest request = new StringRequest("http://udn.com/rssfeed/news/1",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("NET", response);
+                    }
+                }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("NET", "Error:" + error.toString());
+            }
+        });
+
+        queue.add(request);
+        queue.start();
+
     }
 }
