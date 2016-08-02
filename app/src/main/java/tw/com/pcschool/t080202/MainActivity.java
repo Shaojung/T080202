5259597e7cce1df104f930e5ed2d175001ab58ce
shaojung <shaojung@gmail.com>
2016-08-02 14:49:11 +0800

    Volley step 2

--- app/src/main/java/tw/com/pcschool/t080202/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/t080202/MainActivity.java
@@ -19,10 +19,10 @@ public class MainActivity extends AppCompatActivity {
 
         RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
         // String url = "http://data.taipei/opendata/datalist/datasetMeta/download?id=6556e1e8-c908-42d5-b984-b3f7337b139b&rid=55ec6d6e-dc5c-4268-a725-d04cc262172b";
-        // String url = "http://udn.com/rssfeed/news/1";
+        String url = "http://udn.com/rssfeed/news/1";
         // String url = "http://data.ntpc.gov.tw/od/data/api/E09B35A5-A738-48CC-B0F5-570B67AD9C78?$format=json";
-        String url = "http://data.ntpc.gov.tw/od/data/api/B1464EF0-9C7C-4A6F-ABF7-6BDF32847E68?$format=json";
-        StringRequest request = new StringRequest(url,
+        // String url = "http://data.ntpc.gov.tw/od/data/api/B1464EF0-9C7C-4A6F-ABF7-6BDF32847E68?$format=json";
+        StringRequest request = new UTF8StringRequest(url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
